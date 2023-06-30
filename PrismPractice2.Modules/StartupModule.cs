11a5eca5c769c654bb24967c9b66cbc18a5edd90
Thenik9707 <98966752+Thenik9707@users.noreply.github.com>
2023-06-30 11:28:14 +0530

    Prism Application UI

---
+++ PrismPractice2.Modules/StartupModule.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismPractice2.Modules.ViewModels;
+using PrismPractice2.Modules.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismPractice2.Modules
+{
+    public class StartupModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //View Register Only
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("LoginRegion", typeof(LoginView));
+            regionManager.RegisterViewWithRegion("DashboardRegion", typeof(DashboardView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //View Register Only
+            containerRegistry.RegisterForNavigation<LoginView, LoginViewModel>();
+            containerRegistry.RegisterForNavigation<DashboardView, DashboardViewModel>();
+        }
+
+
+    }
+}
